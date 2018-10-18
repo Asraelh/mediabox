9f36fde5a82b7fca7c795c82613864ce936fd2f0
admin <admin@pcenterjava-09>
2018-10-18 17:25:46 +0200

    dhtdrhsdghsdgh

--- src/main/java/mediabox/dao/IUsuarioRepository.java
+++ src/main/java/mediabox/dao/IUsuarioRepository.java
@@ -3,6 +3,7 @@ package mediabox.dao;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.query.Procedure;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
@@ -11,13 +12,13 @@ import mediabox.model.Usuario;
 
 public interface IUsuarioRepository extends CrudRepository<Usuario, String> {
 	
-
-	@Query(value="CALL INSERTUSER(:username,:pass,:email,:alias,:nombre,:apellidos)",nativeQuery=true)
-	public void insertUser(@Param("username") String user, @Param("pass") String password, @Param("email")String email, @Param("alias")String alias, @Param("nombre")String nombre, @Param("apellidos")String apellidos);
+	@Query(value="SELECT insertUser(:username,:pass,:email,:alias,:nombre,:apellidos)",nativeQuery=true)
+	public boolean insertUser(@Param("username") String user, @Param("pass") String password, @Param("email")String email, @Param("alias")String alias, @Param("nombre")String nombre, @Param("apellidos")String apellidos);
 	
 	@Query(value="SELECT CHECKUSER(:p_username,:p_password )",nativeQuery=true)
 	public Boolean checkUser(@Param("p_username") String username,@Param("p_password") String password);
 
-	
+	@Query(value="SELECT CHECKEMAIL(:p_email)",nativeQuery=true)
+	public Boolean checkEmail(@Param("p_email") String email);
 
 }
