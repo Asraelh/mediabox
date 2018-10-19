f0702da5a12ef0c74ddeb6ed1629383ff35cbc26
admin <admin@pcenterjava-09>
2018-10-19 11:52:22 +0200

    fyuk

--- src/main/java/mediabox/dao/IPruebaRepository.java
+++ src/main/java/mediabox/dao/IPruebaRepository.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.query.Procedure;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
@@ -14,7 +15,7 @@ import mediabox.services.*;
 public interface IPruebaRepository extends CrudRepository<Prueba, Integer> {
 
 	
-	@Query(value="CALL pruebaselect()",nativeQuery=true)
+	@Procedure(name = "pruebaselect")
 	public Optional<List<Prueba>> pruebaselect();
 
 }
