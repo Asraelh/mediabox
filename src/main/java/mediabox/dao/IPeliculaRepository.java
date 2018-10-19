795d67fbdbee7fc27d972eb3f2ce2e8eb5a37799
admin <admin@pcenterjava-09>
2018-10-19 13:16:58 +0200

    IPELICULAREPOSITORY

--- src/main/java/mediabox/dao/IPeliculaRepository.java
+++ src/main/java/mediabox/dao/IPeliculaRepository.java
@@ -1,11 +1,19 @@
 package mediabox.dao;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.query.Procedure;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import mediabox.model.Pelicula;
+import mediabox.model.Prueba;
 
 
 
 public interface IPeliculaRepository extends CrudRepository<Pelicula, Integer> {
 
+	@Procedure(name = "SHOW5PELICULAS")
+	public Optional<List<Pelicula>> show5peliculas();
 }
